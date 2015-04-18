389ec506207f2c4a8ccedcdbb657aba342befab2
ragingwind <ragingwind@gmail.com>
2015-04-19 07:51:35 +0900

    Manage WebComponentsReady event

--- samples/debug.js
+++ samples/debug.js
@@ -2,16 +2,20 @@
 
   function showDebugMenu() {
     var debugMenu = document.createElement('div');
+    var baseUrl = 'https://github.com/ragingwind/polymer-samples/blob/master/samples/';
+    var redirectUrl = baseUrl + location.href.split('/').pop();
+
     debugMenu.innerHTML = [
       '<hr>',
       '<div>',
-      '<sub><a href="#" onclick="showSource()">Show source to console</a></sub>',
+      '<sub><a target="blank" href="' + redirectUrl + '">Go to source at github</a></sub>',
       '</div>'
     ].join('\n');
     document.body.appendChild(debugMenu);
   }
 
   this.showSource = function() {
+    console.log(location.href);
     console.log(document.body.innerHTML);
   };
 

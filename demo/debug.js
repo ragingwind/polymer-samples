d40d49d3b0f8a01fb5b9bdd73d7b29a75d70f534
ragingwind <ragingwind@gmail.com>
2015-06-03 07:31:05 -0700

    Change sample file path

---
+++ demo/debug.js
@@ -0,0 +1,26 @@
+(function() {
+
+  function showDebugMenu() {
+    var debugMenu = document.createElement('div');
+    var baseUrl = 'https://github.com/ragingwind/polymer-samples/blob/master/samples/';
+    var redirectUrl = baseUrl + location.href.split('/').pop();
+
+    debugMenu.innerHTML = [
+      '<hr>',
+      '<div>',
+      '<sub><a target="blank" href="' + redirectUrl + '">Go to source at github</a></sub>',
+      '</div>'
+    ].join('\n');
+    document.body.appendChild(debugMenu);
+  }
+
+  this.showSource = function() {
+    console.log(location.href);
+    console.log(document.body.innerHTML);
+  };
+
+  document.addEventListener('WebComponentsReady', function() {
+    showDebugMenu();
+  })
+
+}.call(this));
